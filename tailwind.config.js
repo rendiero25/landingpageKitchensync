741c2268dfaa8bf396c68c69a8991353acd5c1d6
Rendiero25 <workspace.rendy@gmail.com>
2024-11-03 23:13:53 +0700

    header mobile done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./script.js",
+    ".src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      primary: ["Roboto", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
