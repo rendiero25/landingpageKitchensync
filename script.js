d577e2cf720f7368ba66b64c036901a37da968e5
Rendiero25 <workspace.rendy@gmail.com>
2024-11-07 22:09:24 +0700

    All section for mobile done

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ menuButton.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden');
 });
 
+// COUNTDOWN TIMER - START
+// Set the date we're counting down to
+let countDownDate = new Date("Dec 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function() {
+  // Get today's date and time
+  let now = new Date().getTime();
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+  // Calculate days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("days").innerHTML = (days < 10 ? '0' : '') + days;
+  document.getElementById("hours").innerHTML = (hours < 10 ? '0' : '') + hours;
+  document.getElementById("minutes").innerHTML = (minutes < 10 ? '0' : '') + minutes;
+  document.getElementById("seconds").innerHTML = (seconds < 10 ? '0' : '') + seconds;
+
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Pendaftaran Telah Ditutup";
+  }
+}, 1000);
+// COUNTDOWN TIMER - END
